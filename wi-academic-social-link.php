7589c469ea37386daa6f6a688f03e0b5c6f14e0f
Urszula Wielgosz <urszula@wielgosz.info>
2024-04-02 10:36:17 +0000

    Switch to yarn, add dev container

--- wi-academic-social-link.php
+++ wi-academic-social-link.php
@@ -19,6 +19,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 require_once __DIR__ . '/build/index.php';
 
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
 function wi_academic_social_link_block_init() {
 	register_block_type( __DIR__ . '/build', array(
 		'render_callback' => 'wi_academic_social_link_render_block',
@@ -26,8 +33,10 @@ function wi_academic_social_link_block_init() {
 }
 add_action( 'init', 'wi_academic_social_link_block_init' );
 
+/**
+ * Registers the block's styles manually to ensure they are enqueued after the social links block styles.
+ */
 function wi_academic_social_link_block_register_styles() {
-	// register styles manually to ensure they are enqueued after the social links block styles
 	wp_register_style(
 		'wielgosz-info-academic-social-link',
 		plugins_url( 'build/style-index.css', __FILE__ ),
@@ -39,6 +48,12 @@ function wi_academic_social_link_block_register_styles() {
 }
 add_action( 'enqueue_block_assets', 'wi_academic_social_link_block_register_styles' );
 
+/**
+ * Adds the Academic Social Link block to the list of allowed blocks in the Social Links block.
+ *
+ * @param array $metadata The block type metadata.
+ * @return array The block type metadata.
+ */
 function wi_add_academic_social_link_to_social_links( $metadata ) {
 	if ( $metadata['name'] === 'core/social-links' ) {
 		if ( ! in_array( 'wielgosz-info/wi-academic-social-link', $metadata['allowedBlocks'] ) ) {
