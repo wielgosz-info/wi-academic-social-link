a71e4642be39b77e73af369dd8c19eae7166f2db
Urszula Wielgosz <urszula@wielgosz.info>
2024-04-03 18:50:20 +0000

    Add manual registering after all, but only for FE

--- wi-academic-social-link.php
+++ wi-academic-social-link.php
@@ -34,6 +34,24 @@ function wi_academic_social_link_block_init() {
 add_action( 'init', 'wi_academic_social_link_block_init' );
 
 /**
+ * Registers the block's styles manually to ensure they are enqueued after the social links block styles.
+ */
+function wi_academic_social_link_block_register_styles() {
+	if ( ! is_admin() && wp_style_is( 'wielgosz-info-wi-academic-social-link-style', 'registered' ) ) {
+		wp_deregister_style('wielgosz-info-wi-academic-social-link-style');
+		wp_register_style(
+			'wielgosz-info-wi-academic-social-link-style',
+			plugins_url( 'build/style-index.css', __FILE__ ),
+			array(
+				'wp-block-social-links'
+			),
+			filemtime( plugin_dir_path( __FILE__ ) . 'build/style-index.css' )
+		);
+	}
+}
+add_action( 'enqueue_block_assets', 'wi_academic_social_link_block_register_styles', 1 );
+
+/**
  * Adds the WI Academic Social Link block to the list of allowed blocks in the Social Links block.
  *
  * @param array $metadata The block type metadata.
