c645792d739a85b1af1a37abf941244899b4b879
Urszula Wielgosz <urszula@wielgosz.info>
2024-04-03 17:56:16 +0000

    Fix styles loading

--- wi-academic-social-link.php
+++ wi-academic-social-link.php
@@ -34,21 +34,6 @@ function wi_academic_social_link_block_init() {
 add_action( 'init', 'wi_academic_social_link_block_init' );
 
 /**
- * Registers the block's styles manually to ensure they are enqueued after the social links block styles.
- */
-function wi_academic_social_link_block_register_styles() {
-	wp_register_style(
-		'wielgosz-info-academic-social-link',
-		plugins_url( 'build/style-index.css', __FILE__ ),
-		array(
-			'wp-block-social-links'
-		),
-		filemtime( plugin_dir_path( __FILE__ ) . 'build/style-index.css' )
-	);
-}
-add_action( 'enqueue_block_assets', 'wi_academic_social_link_block_register_styles' );
-
-/**
  * Adds the WI Academic Social Link block to the list of allowed blocks in the Social Links block.
  *
  * @param array $metadata The block type metadata.
